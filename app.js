b616ebfa4cc253464a2c9bee51238ad8a4006495
Alexander Ljungqvist <al.ljungqvist@gmail.com>
2018-11-20 14:37:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
 const { NerManager } = require('node-nlp');
 
 const manager = new NerManager({ threshold: 0.8 });
-manager.addNamedEntityText(
-  'hero',
-  'spiderman',
-  ['en'],
-  ['Spiderman', 'Spider-man'],
-);
-manager.addNamedEntityText(
-  'hero',
-  'iron man',
-  ['en'],
-  ['iron man', 'iron-man'],
-);
-manager.addNamedEntityText('hero', 'thor', ['en'], ['Thor']);
-manager.addNamedEntityText(
-  'food',
-  'burguer',
-  ['en'],
-  ['Burguer', 'Hamburguer'],
-);
-manager.addNamedEntityText('food', 'pizza', ['en'], ['pizza']);
-manager.addNamedEntityText('food', 'pasta', ['en'], ['Pasta', 'spaghetti']);
+manager.addNamedEntityText('skill','nodejs',['en'],['node-js', 'node js']);
 manager.findEntities(
-  'I saw spederman eating speghetti in the city',
+  'I saw nodejs eating speghetti in the city',
   'en',
 ).then(entities => {
   console.log(entities)
