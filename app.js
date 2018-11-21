036f7cc9213e9a7a4d45f288f84f78d888c2a81b
JHegelund <hegelund.jacob@gmail.com>
2018-11-21 10:25:31 +0100

    Updated entities and added skillList as result

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const { NerManager } = require('node-nlp');
 
 const manager = new NerManager({ threshold: 0.8 });
-manager.addNamedEntityText('skill','nodejs',['en'],['node-js', 'node js']);
+manager.addNamedEntityText('skill','NodeJS',['en'],['nodejs', 'node']);
+manager.addNamedEntityText('skill','Java',['en'],['java']);
+manager.addNamedEntityText('skill','Javascript',['en'],['javascript']);
+manager.addNamedEntityText('skillGroup','Webb',['en'],['webb']);
 manager.findEntities(
-  'I saw nodejs eating speghetti in the city',
+  'This project needs someone who can nodejs for webb, and NodeJS is very important. Thats why I like node. YEAH NODE. And it also needs java and javaScript',
   'en',
 ).then(entities => {
-  console.log(entities)
+  const skillList = entities.reduce((skills, entity) => {
+    skills[entity.option] = entity.entity;
+    return skills;
+  }, {});
+  console.log(skillList);
 })
