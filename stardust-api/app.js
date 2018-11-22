983c99eb8b9953c649cda87dc46e859d27de6b4c
JHegelund <hegelund.jacob@gmail.com>
2018-11-22 13:59:15 +0100

    Clean up

--- stardust-api/app.js
+++ stardust-api/app.js
@@ -41,24 +41,6 @@ function setSkillEntities(description){
   });
 }
 
-/*manager.addNamedEntityText('skillGroup','Database',['en'],['database']);
-manager.addNamedEntityText('skill','NodeJS',['en'],['nodejs', 'node']);
-manager.addNamedEntityText('skill','Java',['en'],['java']);
-manager.addNamedEntityText('skill','Javascript',['en'],['javascript']);
-manager.addNamedEntityText('skill','GUI',['en'],['gui', 'interface']);
-manager.addNamedEntityText('skillGroup','Architecture',['en'],['architecture']);
-manager.addNamedEntityText('skillGroup','Persistence',['en'],['persistence']);
-manager.addNamedEntityText('skillGroup','Big Data',['en'],['big data']);
-manager.addNamedEntityText('skillGroup','Cloud',['en'],['cloud']);
-manager.addNamedEntityText('skillGroup','Web',['en'],['webb']);
-manager.addNamedEntityText('skillGroup','IOT',['en'],['iot', 'IoT']);
-manager.addNamedEntityText('skillGroup','Lean and Agile',['en'],['lean and agile']);
-manager.addNamedEntityText('skillGroup','Design',['en'],['design']);
-manager.addNamedEntityText('skillGroup','Tools',['en'],['tools']);
-manager.addNamedEntityText('skillGroup','Security',['en'],['security']);
-manager.addNamedEntityText('skillGroup','Web',['en'],['webb']);*/
-
-
 async function fetchStardustData(){
   const options = {
       'method': 'GET',
